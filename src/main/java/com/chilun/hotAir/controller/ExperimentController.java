d519aa583b948e2ca88c2e5799d88afb1289cb2f
齿轮 <2265501251@qq.com>
2025-04-01 18:29:00 +0800

    完成写入流程的开发

--- src/main/java/com/chilun/hotAir/controller/ExperimentController.java
+++ src/main/java/com/chilun/hotAir/controller/ExperimentController.java
@@ -2,19 +2,20 @@ package com.chilun.hotAir.controller;
 
 import com.chilun.hotAir.Utils.ResultUtils;
 import com.chilun.hotAir.facade.ClosedLoopControlFacade;
+import com.chilun.hotAir.model.MachineAdjustableParam;
 import com.chilun.hotAir.model.SystemInitParam;
 import com.chilun.hotAir.model.context.ExperimentContext;
 import com.chilun.hotAir.model.dto.BaseResponse;
 import com.chilun.hotAir.model.dto.experiment.ExperimentAdjustRecordRequest;
+import com.chilun.hotAir.model.entity.ExperimentDataRecord;
 import io.swagger.annotations.Api;
 import io.swagger.v3.oas.annotations.Operation;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
+import java.time.LocalTime;
+import java.util.List;
 
 /**
  * @author 齿轮
@@ -29,10 +30,10 @@ public class ExperimentController {
 
     @PostMapping("/init")
     @Operation(summary = "实验初始化")
-    public BaseResponse<Void> init(HttpSession session, @RequestBody SystemInitParam initParam) {
+    public BaseResponse<Long> init(HttpSession session, @RequestBody SystemInitParam initParam) {
         ExperimentContext context = facade.init(initParam);
         session.setAttribute("context", context);
-        return ResultUtils.success(null);
+        return ResultUtils.success(context.getExperimentId());
     }
 
     @PostMapping("/next")
@@ -51,4 +52,34 @@ public class ExperimentController {
         return ResultUtils.success(null);
     }
 
+    @PostMapping("/end")
+    @Operation(summary = "结束实验")
+    public BaseResponse<Void> end(HttpSession session) {
+        ExperimentContext context = (ExperimentContext) session.getAttribute("context");
+        facade.end(context);
+        return ResultUtils.success(null);
+    }
+
+    @GetMapping("/now")
+    @Operation(summary = "获得当前时间戳的入参出参")
+    public BaseResponse<ExperimentDataRecord> now(HttpSession session) {
+        ExperimentContext context = (ExperimentContext) session.getAttribute("context");
+        return ResultUtils.success(facade.getCurrentInfo(context));
+    }
+
+    @GetMapping("/suggest")
+    @Operation(summary = "获得PSO推荐的修改入参")
+    public BaseResponse<MachineAdjustableParam> suggest(HttpSession session) {
+        ExperimentContext context = (ExperimentContext) session.getAttribute("context");
+        return ResultUtils.success(facade.getSuggest(context));
+    }
+
+    @GetMapping("/history")
+    @Operation(summary = "查询当前实验的历史数据")
+    public BaseResponse<List<ExperimentDataRecord>> history(HttpSession session, @RequestParam String start, @RequestParam String end) {
+        LocalTime startTime = LocalTime.parse(start);
+        LocalTime endTime = LocalTime.parse(end);
+        ExperimentContext context = (ExperimentContext) session.getAttribute("context");
+        return ResultUtils.success(facade.getHistoryInfo(context, startTime, endTime));
+    }
 }
--- src/main/java/com/chilun/hotAir/facade/HistoryRunRecodeFacade.java
+++ src/main/java/com/chilun/hotAir/facade/HistoryRunRecodeFacade.java
@@ -1,11 +1,52 @@
 package com.chilun.hotAir.facade;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.chilun.hotAir.model.entity.Experiment;
+import com.chilun.hotAir.model.entity.ExperimentAdjustRecord;
+import com.chilun.hotAir.model.entity.ExperimentDataRecord;
+import com.chilun.hotAir.service.ExperimentAdjustRecordService;
+import com.chilun.hotAir.service.ExperimentDataRecordService;
+import com.chilun.hotAir.service.ExperimentService;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.List;
+
 /**
  * 历史记录查看外观层
  *
  * @author 齿轮
  * @date 2025-03-22-10:50
  */
-public interface HistoryRunRecodeFacade {
+@Component
+public class HistoryRunRecodeFacade {
+    @Resource
+    ExperimentService experimentService;
+
+    @Resource
+    ExperimentDataRecordService experimentDataRecordService;
+
+    @Resource
+    ExperimentAdjustRecordService experimentAdjustRecordService;
+
+    public List<Experiment> getAllExperiment() {
+        BaseMapper<Experiment> mapper = experimentService.getBaseMapper();
+        return mapper.selectList(new QueryWrapper<>());
+    }
+
+    public List<ExperimentAdjustRecord> getExperimentAdjustRecordByExperiment(long experimentId) {
+        BaseMapper<ExperimentAdjustRecord> mapper = experimentAdjustRecordService.getBaseMapper();
+        QueryWrapper<ExperimentAdjustRecord> wrapper = new QueryWrapper<>();
+        wrapper.eq("experiment_id", experimentId);
+        return mapper.selectList(wrapper);
+    }
+
+    public List<ExperimentDataRecord> getExperimentDataRecordByExperiment(long experimentId) {
+        BaseMapper<ExperimentDataRecord> mapper = experimentDataRecordService.getBaseMapper();
+        QueryWrapper<ExperimentDataRecord> wrapper = new QueryWrapper<>();
+        wrapper.eq("experiment_id", experimentId);
+        return mapper.selectList(wrapper);
+    }
 
 }
